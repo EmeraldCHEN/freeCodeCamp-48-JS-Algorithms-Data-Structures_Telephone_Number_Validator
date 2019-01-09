fc03806d60c8ec7fb7e52b7b60d8aa278d956906
Emerald Chen <ahyuancy@gmail.com>
2019-01-09 19:58:49 +1300

    Update .js

--- .js
+++ .js
@@ -1,10 +1,12 @@
-function
-
-
-
-
-
-
+function telephoneCheck(str) {
+    let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    return regex.test(str);
+}
+// Use ^ to search for patterns at the beginning of strings
+// ... $ .............................  end   .............
+// Use \s?  to search one whitespace or none
+// (\(\d{3}\)|\d{3}) checks for either three digits surrounded by parentheses, or three digits by themselves with no parentheses
+// [\s\-]? checks for spaces or dashes between the groups of digits
 
 
 
